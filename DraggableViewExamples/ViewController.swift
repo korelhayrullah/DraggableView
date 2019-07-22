038c0e730743838aa7ca6ce1339cd26d5f4d96f1
korelhayrullah <korel.hayrullah@gmail.com>
2019-07-22 23:09:51 +0300

    Added example

--- DraggableViewExamples/ViewController.swift
+++ DraggableViewExamples/ViewController.swift
@@ -7,14 +7,34 @@
 //
 
 import UIKit
+import DraggableView
 
 class ViewController: UIViewController {
-
+  
+  var draggableView = DraggableView()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    setDraggableView()
   }
-
-
+  
+  func setDraggableView() {
+    draggableView.translatesAutoresizingMaskIntoConstraints = false
+    draggableView.backgroundColor = .darkGray
+    
+    view.addSubview(draggableView)
+    
+    draggableView.padding = 10
+    draggableView.threshold = 1500
+    
+    let screenSize = UIScreen.main.bounds
+    
+    NSLayoutConstraint.activate([
+      draggableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: draggableView.padding),
+      draggableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -draggableView.padding),
+      draggableView.heightAnchor.constraint(equalToConstant: screenSize.height * 0.4),
+      draggableView.widthAnchor.constraint(equalToConstant: screenSize.width * 0.5)
+      ])
+  }
 }
 
