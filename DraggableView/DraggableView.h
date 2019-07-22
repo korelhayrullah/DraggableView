760a3b01ea5bd4d28f593953e4b452d0888ae46b
korelhayrullah <korel.hayrullah@gmail.com>
2019-07-22 21:47:11 +0300

    Initial project setup

---
+++ DraggableView/DraggableView.h
@@ -0,0 +1,19 @@
+//
+//  DraggableView.h
+//  DraggableView
+//
+//  Created by Korel Hayrullah on 22.07.2019.
+//  Copyright © 2019 Korel Hayrullah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DraggableView.
+FOUNDATION_EXPORT double DraggableViewVersionNumber;
+
+//! Project version string for DraggableView.
+FOUNDATION_EXPORT const unsigned char DraggableViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DraggableView/PublicHeader.h>
+
+
---
+++ DraggableViewExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DraggableViewExamples
+//
+//  Created by Korel Hayrullah on 22.07.2019.
+//  Copyright © 2019 Korel Hayrullah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
